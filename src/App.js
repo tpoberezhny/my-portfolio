6fd0399ced5d8fb38f1c060e3802b0b5adcd992d
Tim <poberezhny003@gmail.com>
2023-05-22 04:23:59 +0200

    Adding Skills part, connecting to the mockAPI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import NavBar from './Components/NavBar';
-import Home from './Components/Home';
-import AboutMe from './Components/AboutMe';
-import Projects from './Components/Projects';
-import ContactMe from './Components/ContactMe';
+import NavBar from "./Components/NavBar";
+import Home from "./Components/Home";
+import AboutMe from "./Components/AboutMe";
+import Projects from "./Components/Projects";
+import ContactMe from "./Components/ContactMe";
 
 function App() {
+  const [skills, setSkills] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://646aa2107d3c1cae4ce2b010.mockapi.io/skills")
+      .then((res) => {
+        setSkills(res.data);
+      });
+  }, []);
+
   return (
-    <div className='wrapper'>
+    <div className="wrapper">
       <NavBar />
       <Home />
-      <AboutMe />
+      <AboutMe skills={skills} />
     </div>
   );
 }
--- src/Components/AboutMe/index.js
+++ src/Components/AboutMe/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import styles from "./AboutMe.module.scss";
 
-function AboutMe() {
+function AboutMe(props) {
+  console.log(props.skills);
   return (
     <div className="aboutMeSection">
-      <img className={styles.laptopImg} src="../../image/laptop.png" alt="Laptop Image"/>
+      <img
+        className={styles.laptopImg}
+        src="../../image/laptop.png"
+        alt="Laptop Image"
+      />
       <div className={styles.title}>
         <h1>
           About <span>Me</span>
@@ -14,6 +19,16 @@ function AboutMe() {
           dolor sit amet, consectetuer adipiscing elit.Lorem ipsum dolor sit
           amet, consectetuer adipiscing elit.
         </h2>
+      </div>
+
+      <div className={styles.skills}>
+          {props.skills.map((item) => (
+            <div className={styles.skill}>
+              <img width={300} height={300} src={item.imageUrl} />
+              <h2>{item.title}</h2>
+              <h2 className={styles.skillDesc}>{item.description}</h2>
+            </div>
+          ))}
       </div>
     </div>
   );
