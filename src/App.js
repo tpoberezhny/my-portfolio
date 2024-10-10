4ad6ecddeb892d3daef61a3aff89061eeede68ba
Tim <poberezhny003@gmail.com>
2024-10-10 16:10:09 +0200

    Adding backEnd to the Skill section. Moving all data from API to the local storage

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import NavBar from "./Components/NavBar";
 import Home from "./Components/Home";
 import AboutMe from "./Components/AboutMe";
 import Projects from "./Components/Projects";
-import DesignProjects from './Components/DesignProjects';
+import DesignProjects from "./Components/DesignProjects";
 import ContactMe from "./Components/ContactMe";
+
+import skillsData from "./data/skills.json";
+import projectsData from "./data/projects.json";
+import designProjectsData from "./data/designProjects.json";
 
 function App() {
   const [skills, setSkills] = useState([]);
@@ -14,28 +18,9 @@ function App() {
   const [designProjects, setDesignProjects] = useState([]);
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const skillsResponse = await axios.get(
-          "https://65fdc1b8b2a18489b385620d.mockapi.io/api/tima/skills"
-        );
-        const projectsResponse = await axios.get(
-          "https://65fdc1b8b2a18489b385620d.mockapi.io/api/tima/projects"
-        );
-        const designProjectsResponse = await axios.get(
-          "https://65fdc1b8b2a18489b385620d.mockapi.io/api/tima/design"
-        );
-
-        setSkills(skillsResponse.data);
-        setProjects(projectsResponse.data);
-        setDesignProjects(designProjectsResponse.data);
-      } catch (error) {
-        alert("Data request error ;(");
-        console.error(error);
-      }
-    }
-
-    fetchData();
+    setSkills(skillsData);
+    setProjects(projectsData);
+    setDesignProjects(designProjectsData);
   }, []);
 
   return (
@@ -43,7 +28,7 @@ function App() {
       <NavBar />
       <Home />
       <AboutMe skills={skills} />
-      <Projects projects={projects}/>
+      <Projects projects={projects} />
       <DesignProjects designProjects={designProjects} />
       <ContactMe />
     </div>
