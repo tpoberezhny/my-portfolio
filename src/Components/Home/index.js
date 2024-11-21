a2101196d880455db69b6a78689685a8f9f54003
Tim <poberezhny003@gmail.com>
2024-11-21 11:39:23 +0100

    Adding new portfolio link

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -16,11 +16,22 @@ function Home() {
       <div className={styles.information}>
         <h1>Hello,</h1>
         <h1>
-          I am a
-          <span> {text} </span>
+          I am a<span> {text} </span>
           <Cursor cursorColor="#7C3AED" />
         </h1>
-        <h2>I am Tim, a passionate Full-Stack Dev | UI/UX Designer. Based in Prague <img src="image/pin.svg" width={30} height={30} alt="Pin"/></h2>
+        <h2>
+          Hey, my new website-portfolio with NextJS are ready, check it out:
+          <br/>
+          <span style={{color: "black"}}>I would love to hear some feedback from you.</span>
+        </h2>
+        <a
+          href="https://tpoberezhny.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.newWeb}
+        >
+          https://tpoberezhny.com
+        </a>
         <Link to="contactMe" spy={true} smooth={true} duration={1000}>
           <button className={styles.hireMe}>
             Hire Me{" "}
@@ -48,7 +59,11 @@ function Home() {
           src="image/homeImage.png"
           alt="Boy with laptop smiling"
         />
-        <img className={styles.mobileHomeImage} src="image/homeMobileImage.png" alt="Mobile version of me"/>
+        <img
+          className={styles.mobileHomeImage}
+          src="image/homeMobileImage.png"
+          alt="Mobile version of me"
+        />
       </div>
     </div>
   );
